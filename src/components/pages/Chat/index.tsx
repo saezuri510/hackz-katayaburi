6dc7245c0ccd1ea0060308dfcfa01f02babdd1b5
Akira_0809 <102137735+Akira0809@users.noreply.github.com>
2023-09-20 01:59:56 +0900

    chatの追加

--- src/components/pages/Chat/index.tsx
+++ src/components/pages/Chat/index.tsx
@@ -1,10 +1,16 @@
 import BackGround from '@/components/ui/BackGround'
+import Box from '@/components/ui/Box'
+import { PopButton } from '@/components/ui/domain/PopButton'
 import React from 'react'
 
 function ChatPage() {
   return (
     <BackGround>
-      <div></div>
+      <Box>
+        <div>
+          <PopButton>次へ</PopButton>
+        </div>
+      </Box>
     </BackGround>
   )
 }
