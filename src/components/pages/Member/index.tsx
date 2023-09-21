0b40c2fb6ff958a1fe0971541a032f885ff5bf24
Akira_0809 <102137735+Akira0809@users.noreply.github.com>
2023-09-21 13:30:23 +0900

    member

--- src/components/pages/Member/index.tsx
+++ src/components/pages/Member/index.tsx
@@ -1,5 +1,5 @@
 import Router from "next/router";
-import React from "react";
+import React, { ChangeEvent, Fragment, useState } from "react";
 import { BsPlayFill } from "react-icons/bs";
 
 import { FrameText } from "@/components/icons/FrameText";
@@ -9,8 +9,26 @@ import { PopButton } from "@/components/ui/domain/PopButton";
 import UserBord from "@/components/ui/domain/UserBord";
 
 const MemberPage = () => {
+  const [UserList, setUserList] = useState<{ name: string; status: boolean }[]>([
+    { name: "Akira", status: true },
+    { name: "Akira", status: true },
+    { name: "Akira", status: true },
+    { name: "Akira", status: true },
+  ]);
+
   const handler = () => {
     Router.push("/theme");
+  };
+
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const value = Number(e.target.value);
+    setUserList((prev) => {
+      if (value < prev.length) {
+        return prev.splice(0, value);
+      }
+      const length = Math.max(prev.length, value - prev.length);
+      return [...prev, ...[...Array(length)].map(() => ({ name: "空", status: false }))];
+    });
   };
 
   return (
@@ -19,12 +37,64 @@ const MemberPage = () => {
         <GameFrame>
           <div className="flex w-96 flex-col items-center">
             <FrameText fillColor="#73EECD" fontSize={100} text="プレイヤー" width={200} />
-            <UserBord status name="Akira" />
+            <select
+              className="rounded-md border border-white bg-inherit text-white"
+              onChange={onChange}
+            >
+              <option className="text-black" value={4}>
+                4プレイヤー
+              </option>
+              <option className="text-black" value={5}>
+                5プレイヤー
+              </option>
+              <option className="text-black" value={6}>
+                6プレイヤー
+              </option>
+              <option className="text-black" value={7}>
+                7プレイヤー
+              </option>
+              <option className="text-black" value={8}>
+                8プレイヤー
+              </option>
+              <option className="text-black" value={9}>
+                9プレイヤー
+              </option>
+              <option className="text-black" value={10}>
+                10プレイヤー
+              </option>
+              <option className="text-black" value={12}>
+                12プレイヤー
+              </option>
+              <option className="text-black" value={14}>
+                14プレイヤー
+              </option>
+              <option className="text-black" value={16}>
+                16プレイヤー
+              </option>
+              <option className="text-black" value={18}>
+                18プレイヤー
+              </option>
+              <option className="text-black" value={20}>
+                20プレイヤー
+              </option>
+              <option className="text-black" value={30}>
+                30プレイヤー
+              </option>
+              <option className="text-black" value={50}>
+                50プレイヤー
+              </option>
+            </select>
+            {UserList.map(({ name, status }, index) => (
+              <Fragment key={index}>
+                <UserBord name={name} status={status} />
+              </Fragment>
+            ))}
+            {/* <UserBord status name="Akira" />
             <UserBord name="空" status={false} />
             <UserBord name="空" status={false} />
             <UserBord name="空" status={false} />
             <UserBord name="空" status={false} />
-            <UserBord name="空" status={false} />
+            <UserBord name="空" status={false} /> */}
             <div className="pt-3">
               <PopButton onClick={handler}>
                 <BsPlayFill />
